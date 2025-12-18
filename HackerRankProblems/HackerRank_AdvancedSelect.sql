98d25e1846b429122b35f9860598d11e34e338d9
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2025-12-18 21:16:46 +0530

    Update HackerRank_AdvancedSelect.sql

--- HackerRankProblems/HackerRank_AdvancedSelect.sql
+++ HackerRankProblems/HackerRank_AdvancedSelect.sql
@@ -17,3 +17,45 @@ from triangles;
 
 ---
 
+/*
+Problem: Occupations
+- You are given a table named OCCUPATIONS with the following structure:
+  Name        STRING
+  Occupation  STRING
+
+- The Occupation column will contain only one of the following values:
+Doctor, Professor, Singer, or Actor.
+Task:
+- Generate the following two result sets:
+1) Query an alphabetically ordered list of all names in the OCCUPATIONS table.
+    Each name should be immediately followed by the first letter of the occupation in parentheses.
+        - Example format: Name(FirstLetterOfOccupation)
+
+2) Query the number of occurrences of each occupation in the OCCUPATIONS table.
+    Output the result in the following format:
+    There are a total of [occupation_count] [occupation]s.
+
+Sorting rules for the second query:
+- Sort by occupation_count in ascending order.
+- If counts are the same, sort alphabetically by occupation name.
+
+Note:
+- There will be at least two entries for each occupation type.
+*/
+
+-- 1.
+select concat(name, "(", left(occupation, 1), ")")
+from occupations
+order by name;
+
+-- 2.
+select concat(
+           "There are a total of ",
+           count(*),
+           " ",
+           lower(occupation),
+           "s."
+       )
+from occupations
+group by occupation
+order by count(*), occupation;
