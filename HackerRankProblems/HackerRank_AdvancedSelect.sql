4907a87321103542b62913ff4744515d8520e3c4
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2025-12-21 22:55:55 +0530

    Update HackerRank_AdvancedSelect.sql

--- HackerRankProblems/HackerRank_AdvancedSelect.sql
+++ HackerRankProblems/HackerRank_AdvancedSelect.sql
@@ -92,13 +92,12 @@ Root: If node is root node.
 Leaf: If node is leaf node.
 Inner: If node is neither root nor leaf node.
 */
-select
-    n,
-    case
-        when p is null then 'Root'
-        when n not in (select distinct p from bst where p is not null) then 'Leaf'
-        else 'Inner'
-    end as node_type
+select n,
+case
+    when p is null then 'Root'
+    when n not in (select distinct p from bst where p is not null) then 'Leaf'
+    else 'Inner'
+end as node_type
 from bst
 order by n;
 
