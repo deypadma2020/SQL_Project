b259a980079887af750d4d0cbc4b6bde643c93ec
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2026-01-01 21:11:36 +0530

    Update HackerRank_Aggregation.sql

--- HackerRankProblems/HackerRank_Aggregation.sql
+++ HackerRankProblems/HackerRank_Aggregation.sql
@@ -98,3 +98,16 @@ where total_earnings = global_max
 group by total_earnings;
 
 ---
+
+/*
+Query the following two values from the STATION table:
+
+    1. The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+    2. The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+*/
+select
+    round(sum(lat_n), 2) as sum_lat_n,
+    round(sum(long_w), 2) as sum_long_w
+from station;
+
+---
