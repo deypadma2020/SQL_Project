47cee624d9a502fdfecf01c1ef3747d8eeb98429
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2026-01-02 21:01:45 +0530

    Update HackerRank_Aggregation.sql

--- HackerRankProblems/HackerRank_Aggregation.sql
+++ HackerRankProblems/HackerRank_Aggregation.sql
@@ -111,3 +111,12 @@ select
 from station;
 
 ---
+
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+*/
+select truncate(sum(lat_n), 4) as sum_lat_n
+from station
+where lat_n>38.7880 and lat_n<137.2345;
+
+---
