ab50ed77e1ad83ab3fd65778148a9fed0ff3798e
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2026-01-04 15:04:37 +0530

    Update HackerRank_Aggregation.sql

--- HackerRankProblems/HackerRank_Aggregation.sql
+++ HackerRankProblems/HackerRank_Aggregation.sql
@@ -129,3 +129,17 @@ from station
 where lat_n<137.2345;
 
 ---
+
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places.
+*/
+with max_lat as(
+    select max(lat_n) as max_lat_n
+    from station
+    where lat_n < 137.2345
+)
+select round(long_w, 4) as long_w
+from station as s 
+inner join max_lat as ml on s.lat_n = ml.max_lat_n;
+
+---
