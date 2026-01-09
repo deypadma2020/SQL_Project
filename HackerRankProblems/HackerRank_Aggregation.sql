4c41321406864e086d03a0e03788cf2bdfa4c249
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2026-01-09 09:58:14 +0530

    Update HackerRank_Aggregation.sql

--- HackerRankProblems/HackerRank_Aggregation.sql
+++ HackerRankProblems/HackerRank_Aggregation.sql
@@ -221,3 +221,21 @@ from station;
 
 ---
 
+/*
+A median is defined as a number separating the higher half of a data set from the lower half. 
+Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places.
+*/
+with ordered_latitudes as (
+    select lat_n,
+        row_number() over (order by lat_n) as rn,
+        count(*) over () as total_rows
+        from station
+)
+select round(avg(lat_n), 4) as median_latitude
+from ordered_latitudes
+where rn in (
+    (total_rows + 1) / 2,
+    (total_rows + 2) / 2
+);
+
+---
