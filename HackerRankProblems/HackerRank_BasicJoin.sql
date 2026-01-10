5b8802e616d85449f3b1a2349cc9d93157a39c61
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2026-01-10 10:33:50 +0530

    Update HackerRank_BasicJoin.sql

--- HackerRankProblems/HackerRank_BasicJoin.sql
+++ HackerRankProblems/HackerRank_BasicJoin.sql
@@ -3,3 +3,9 @@ Given the CITY and COUNTRY tables, query the sum of the populations of all citie
 
 Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 */
+select sum(ci.population) as total_population
+from city as ci
+inner join country as co on ci.countrycode = co.code
+where co.continent = "Asia";
+
+---
