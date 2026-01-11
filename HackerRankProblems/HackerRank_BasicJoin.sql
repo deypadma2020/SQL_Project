8f9412e0dfedbd3419ce35077f694b45ca5dd3bb
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2026-01-11 11:02:18 +0530

    Update HackerRank_BasicJoin.sql

--- HackerRankProblems/HackerRank_BasicJoin.sql
+++ HackerRankProblems/HackerRank_BasicJoin.sql
@@ -9,3 +9,15 @@ inner join country as co on ci.countrycode = co.code
 where co.continent = "Asia";
 
 ---
+
+/*
+Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+select ci.name
+from city ci
+inner join country co on ci.countrycode = co.code 
+where co.continent = "Africa";
+
+---
