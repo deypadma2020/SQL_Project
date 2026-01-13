0ac5cd41df130c1d69290adaebac0186ee728f43
Padma Dey <125171579+deypadma2020@users.noreply.github.com>
2026-01-13 11:08:30 +0530

    Update HackerRank_BasicJoin.sql

--- HackerRankProblems/HackerRank_BasicJoin.sql
+++ HackerRankProblems/HackerRank_BasicJoin.sql
@@ -33,3 +33,51 @@ inner join city ci on co.code = ci.countrycode
 group by co.continent;
 
 ---
+
+/*
+You are given two tables: students and grades.
+
+The students table contains the following columns:
+- id (integer)
+- name (string)
+- marks (integer)
+
+The grades table contains the following columns:
+- grade
+- min_mark
+- max_mark
+
+Each student is assigned a grade based on their marks falling between min_mark and max_mark (inclusive).
+
+Write a query to generate a report with the following columns:
+- name
+- grade
+- marks
+
+Requirements:
+- If a student receives a grade lower than 8, display "NULL" as the name.
+- If a student receives a grade of 8 or higher, display the student's actual name.
+- Sort the results by grade in descending order.
+- For students with the same grade:
+  - If the grade is 8 or higher, sort by name alphabetically.
+  - If the grade is lower than 8, sort by marks in ascending order.
+*/
+select
+case
+    when g.grade < 8 then 'NULL'
+    else s.name
+end as name,
+g.grade, s.marks
+from students s
+inner join grades g on s.marks between g.min_mark and g.max_mark
+order by g.grade desc,
+case
+    when g.grade >= 8 then s.name
+    else null
+end asc,
+case
+    when g.grade < 8 then s.marks
+    else null
+end asc;
+
+---
